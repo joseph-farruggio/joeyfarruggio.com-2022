7186a988b14b97eaf07c4c4aac74d7c81e4ad72a
Joey Farruggio <joey@joeyfarruggio.com>
2021-12-24 13:19:15 -0600

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -21,54 +21,16 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.setLibrary("md", markdownLib);
 
-
-    // /* Markdown Overrides */
-    // let markdownLibrary = markdownIt({
-    //   html: true,
-    //   breaks: true,
-    //   linkify: true
-    // }).use(markdownItAnchor, {
-    //   permalink: true,
-    //   permalinkClass: "direct-link",
-    //   permalinkSymbol: "#"
-    // });
-    // eleventyConfig.setLibrary("md", markdownLibrary);
-        
-
-    // Alias `layout: post` to `layout: layouts/post.njk`
-    // eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
-
-    // function filterTagList(tags) {
-    // return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-    // }
-
-    // eleventyConfig.addFilter("filterTagList", filterTagList);
-
-    // eleventyConfig.addCollection("blog", function(collection) {
-    //     return collection.getFilteredByGlob("blog/*.md");
-    // });
-
-    // // Create an array of all tags
-    // eleventyConfig.addCollection("tagList", function(collection) {
-    //     let tagSet = new Set();
-    //     collection.getAll().forEach(item => {
-    //     (item.data.tags || []).forEach(tag => tagSet.add(tag));
-    //     });
-
-    //     return filterTagList([...tagSet]);
-    // });
-
     eleventyConfig.addFilter("readableDate", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
     });
     
-      // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-      });
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
 
     return {
-        pathPrefix: "https://joseph-farruggio.github.io/joeyfarruggio.com-2022",
         dir: {
             input: "src",
             output: "public"
