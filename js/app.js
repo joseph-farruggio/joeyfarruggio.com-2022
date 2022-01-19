b2d957a0339f1b4ebe4d5e55cb2e0c82f4f7bbda
Joey Farruggio <joey@joeyfarruggio.com>
2022-01-19 10:59:09 -0600

    Updates

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 import Alpine from 'alpinejs'
 import intersect from '@alpinejs/intersect'
- 
-Alpine.plugin(intersect)
+import persist from '@alpinejs/persist'
+
+Alpine.plugin(intersect);
+Alpine.plugin(persist);
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('app', () => ({
+        dark: true,
+        showAll: false,
+
+        showTestimonials() {
+            this.showAll = !this.showAll;
+            if (!this.showAll) {
+                document.getElementById('testimonials').scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        },
+
+        toggleTheme() {
+            if (window.localStorage.theme === 'dark') {
+                document.documentElement.classList.remove('dark');
+                window.localStorage.theme = null;
+            } else {
+                document.documentElement.classList.add('dark');
+                window.localStorage.theme = 'dark';
+            }
+
+            this.dark = !this.dark;
+        },
+
+        init() {
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                this.dark = true;
+            } else {
+                this.dark = false;
+            }
+        }
+    }))
+})
 
 window.Alpine = Alpine
- 
 Alpine.start()
