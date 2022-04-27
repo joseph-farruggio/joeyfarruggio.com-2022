2616d523df961f613c2639b3ab0b5114049b2a13
Joey Farruggio <joey@joeyfarruggio.com>
2022-04-27 13:07:40 -0500

    new post and theme styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,54 @@
-import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect'
-import persist from '@alpinejs/persist'
-import initVimeoPlayer from './components/VimeoPlayer';
+import Alpine from "alpinejs";
+import intersect from "@alpinejs/intersect";
+import persist from "@alpinejs/persist";
+import initVimeoPlayer from "./components/VimeoPlayer";
 
 Alpine.plugin(intersect);
 Alpine.plugin(persist);
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('app', () => ({
-        dark: true,
-        showAll: false,
+document.addEventListener("alpine:init", () => {
+  Alpine.data("app", () => ({
+    dark: true,
+    showAll: false,
 
-        showTestimonials() {
-            this.showAll = !this.showAll;
-            if (!this.showAll) {
-                document.getElementById('testimonials').scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        },
+    showTestimonials() {
+      this.showAll = !this.showAll;
+      if (!this.showAll) {
+        document.getElementById("testimonials").scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    },
 
-        toggleTheme() {
-            if (window.localStorage.theme != 'dark') {
-                document.documentElement.classList.add('dark');
-                localStorage.theme = 'dark';
-            } else {
-                document.documentElement.classList.remove('dark');
-                window.localStorage.theme = null;
-            }
+    toggleTheme() {
+      if (window.localStorage.theme != "dark") {
+        document.documentElement.classList.add("dark");
+        window.localStorage.theme = "dark";
+      } else {
+        document.documentElement.classList.remove("dark");
+        window.localStorage.theme = "light";
+      }
 
-            console.log(window.localStorage.theme);
+      console.log(window.localStorage.theme);
 
-            this.dark = !this.dark;
-        },
+      this.dark = !this.dark;
+    },
 
-        init() {
-            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                this.dark = true;
-            } else {
-                this.dark = false;
-            }
-        }
-    }));
-})
+    init() {
+      if (
+        window.localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        this.dark = true;
+      } else {
+        this.dark = false;
+      }
+    },
+  }));
+});
 
 initVimeoPlayer();
 
-window.Alpine = Alpine
-Alpine.start()
+window.Alpine = Alpine;
+Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,57 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   important: true,
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./src/*.njk",
     "./src/_includes/*.njk",
     "./src/_includes/layouts/*.njk",
-
+    "./src/blog/*.md",
   ],
   theme: {
     fontFamily: {
-      'body': ['Qucksand', 'Helvetica', 'Arial', 'sans-serif'],
+      body: ["Qucksand", "Helvetica", "Arial", "sans-serif"],
     },
     extend: {
-
       colors: {
-        darkBG: '#040817',
+        darkBG: "#040817",
       },
 
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.slate.500"),
+            a: {
+              color: theme("colors.orange.400"),
+              "&:hover": {
+                color: theme("colors.gray.900"),
+              },
+            },
+
+            img: {
+              // borderRadius: defaultTheme('borderRadius.length'),
+              // shadow: defaultTheme('shadow.lg'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            color: theme("colors.white"),
+            a: {
+              color: theme("colors.orange.400"),
+              "&:hover": {
+                color: theme("colors.white"),
+              },
+            },
+            p: {
+              fontSize: theme("fontSize.xs"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
