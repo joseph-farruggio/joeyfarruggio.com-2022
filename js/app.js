d473f95c346ea9afd75d5482fa17f16adcee1785
Joey Farruggio <joey@joeyfarruggio.com>
2022-10-08 14:54:33 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,51 +1,12 @@
 import Alpine from "alpinejs";
 import intersect from "@alpinejs/intersect";
 import persist from "@alpinejs/persist";
-import initVimeoPlayer from "./components/VimeoPlayer";
 
 Alpine.plugin(intersect);
 Alpine.plugin(persist);
 
 document.addEventListener("alpine:init", () => {
-  Alpine.data("app", () => ({
-    dark: true,
-    showAll: false,
-
-    showTestimonials() {
-      this.showAll = !this.showAll;
-      if (!this.showAll) {
-        document.getElementById("testimonials").scrollIntoView({
-          behavior: "smooth",
-        });
-      }
-    },
-
-    toggleTheme() {
-      if (window.localStorage.theme != "dark") {
-        document.documentElement.classList.add("dark");
-        window.localStorage.theme = "dark";
-      } else {
-        document.documentElement.classList.remove("dark");
-        window.localStorage.theme = "light";
-      }
-
-      console.log(window.localStorage.theme);
-
-      this.dark = !this.dark;
-    },
-
-    init() {
-      if (
-        window.localStorage.theme === "dark" ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ) {
-        this.dark = true;
-      } else {
-        this.dark = false;
-      }
-    },
-  }));
+  Alpine.data("app", () => ({}));
 
   Alpine.data("visibleNavHighlighter", () => ({
     headings: undefined,
@@ -108,8 +69,6 @@ document.addEventListener("alpine:init", () => {
 
   Alpine.store("showToc", false);
 });
-
-initVimeoPlayer();
 
 window.Alpine = Alpine;
 Alpine.start();
